2a455fb69f43848953869674adbcf9056a592d93
nikibikini1 <volproekt14@gmail.com>
2025-02-16 13:51:15 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Витрина проектов Е.ИНФО</h1>
+        <p>Здесь будет отображаться список проектов и метрик.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
